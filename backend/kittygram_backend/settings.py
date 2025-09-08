e5b5bec4b4fa96d7d3fcad8c7594cad75bc4c602
Arthur Fissunov <artur-fisunov@mail.ru>
2025-09-08 19:28:17 +0300

    Настройка сервера2323

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -23,6 +23,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'rest_framework',
+    'rest_framework.authtoken',  # ← ДОБАВЬТЕ
 ]
 
 MIDDLEWARE = [
@@ -107,18 +108,46 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated', 
+        'rest_framework.permissions.AllowAny',
     ],
 
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
-
 }
 
 CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://kitykity.ddns.net",
+    "http://localhost:9000",
+    "http://127.0.0.1:9000",
+]
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+# CSRF настройки
+CSRF_TRUSTED_ORIGINS = [
+    "https://kitykity.ddns.net",
+]
+
+# Сессионные куки
+SESSION_COOKIE_SAMESITE = 'None'
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SAMESITE = 'None'
+CSRF_COOKIE_SECURE = True
