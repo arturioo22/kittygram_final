6d517e4548f2d58bdb3f295bba90a2e4c2a16fe3
Arthur Fissunov <artur-fisunov@mail.ru>
2025-09-09 00:12:04 +0300

    Настройка сервера10000

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -34,11 +34,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    #'corsheaders.middleware.CorsMiddleware',  # Должен быть первым!
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
