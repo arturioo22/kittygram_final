062c52114339037544a8c9b73c9cd1521eb342e3
Arthur Fissunov <artur-fisunov@mail.ru>
2025-09-09 21:46:16 +0300

    Настройка сервера10000

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -107,6 +107,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
     ],
