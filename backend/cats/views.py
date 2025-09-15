577e861b05b64e031757b7d6900ce4c24730a762
Arthur Fissunov <artur-fisunov@mail.ru>
2025-09-15 21:52:07 +0300

    Добавлена новая функциональность

--- backend/cats/views.py
+++ backend/cats/views.py
@@ -1,14 +1,25 @@
 from rest_framework import viewsets
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 from .models import Achievement, Cat
 from .serializers import AchievementSerializer, CatSerializer
 
 
+class SimplePagination(PageNumberPagination):
+    page_size = 10
+
+    def get_paginated_response(self, data):
+        return Response({
+            'count': self.page.paginator.count,
+            'results': data
+        })
+
+
 class CatViewSet(viewsets.ModelViewSet):
     queryset = Cat.objects.all()
     serializer_class = CatSerializer
-    pagination_class = PageNumberPagination
+    pagination_class = SimplePagination
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
