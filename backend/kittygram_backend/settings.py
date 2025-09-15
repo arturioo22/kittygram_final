209c0d8d0fbabd72ff191f0287f08d184f137782
Arthur Fissunov <artur-fisunov@mail.ru>
2025-09-16 01:08:36 +0300

    Добавлена новая функциональность

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -14,7 +14,7 @@ DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 CORS_ALLOWED_ORIGINS = os.getenv(
-    'CORS_ALLOWED_ORIGINS', 
+    'CORS_ALLOWED_ORIGINS',
     'http://localhost:3000,http://127.0.0.1:3000'
 ).split(',')
 
