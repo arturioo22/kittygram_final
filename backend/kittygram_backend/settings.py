af8a95ba0474b3a1decdb94b38139d705076bccb
Arthur Fissunov <artur-fisunov@mail.ru>
2025-09-18 13:54:14 +0300

    Добавлена новая функциональность

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,16 +1,17 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+from django.core.management.utils import get_random_secret_key
 
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv('SECRET_KEY', 'fallback-key-for-dev-only')
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
 CORS_ALLOW_ALL_ORIGINS = True
 
